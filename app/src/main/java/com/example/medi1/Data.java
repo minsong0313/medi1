e3214c2072151483c39810a037a09da2398531ca
Minsong Lee <minsong0313@naver.com>
2020-06-03 15:51:43 +0900

    realtime database.

---
+++ app/src/main/java/com/example/medi1/Data.java
@@ -0,0 +1,22 @@
+package com.example.medi1;
+
+import java.util.Map;
+
+public class Data {
+    public String name;
+    public String color;
+    public String shape;
+    public String image;
+
+    public Data(){
+
+    }
+
+    public Data(String name, String color, String shape, String image) {
+        this.name = name;
+        this.color = color;
+        this.shape = shape;
+        this.image = image;
+    }
+
+}
---
+++ app/src/main/java/com/example/medi1/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.medi1;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.opencsv.CSVReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final String REQUIRED = "Required";
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+
+    private TextView textView;
+    private EditText editText;
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRootRef.child("Druglist").orderByKey().addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
+
+
+
+
+}
