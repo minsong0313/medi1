6b0268e7a4e361948331edc80bfc4183d1a28610
Minsong Lee <minsong0313@naver.com>
2020-06-04 16:40:37 +0900

    realtime database.

--- app/src/main/java/com/example/medi1/Data.java
+++ app/src/main/java/com/example/medi1/Data.java
@@ -1,7 +1,5 @@
 package com.example.medi1;
 
-import java.util.Map;
-
 public class Data {
     public String name;
     public String color;
@@ -12,11 +10,45 @@ public class Data {
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    /*
     public Data(String name, String color, String shape, String image) {
         this.name = name;
         this.color = color;
         this.shape = shape;
         this.image = image;
     }
+    */
 
 }
--- app/src/main/java/com/example/medi1/MainActivity.java
+++ app/src/main/java/com/example/medi1/MainActivity.java
@@ -1,75 +1,66 @@
 package com.example.medi1;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.util.Log;
 
-import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.database.ChildEventListener;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.opencsv.CSVReader;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-    private static final String REQUIRED = "Required";
-
-    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-
-    private TextView textView;
-    private EditText editText;
-    private Button button;
-
+    private RecyclerView recylerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Data> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRootRef.child("Druglist").orderByKey().addChildEventListener(new ChildEventListener() {
+        recylerView = findViewById(R.id.recyclerView); //id연결
+        recylerView.setHasFixedSize(true);//리사이클러뷰 기존 성능 강화
+        layoutManager = new LinearLayoutManager(this);
+        recylerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>(); //user 객체 담을 array list(어뎁터 쪽으로 날리려고함)
+
+        database = FirebaseDatabase.getInstance();
+
+        databaseReference = database.getReference("Druglist"); //DB테이블을 연결하는 작업
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                arrayList.clear();//arraylist:유저 객체를 담을것. add한적도 없는데 clear로 초기화?
+                //기존 배열리스트가 존재하지 않게 초기화
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){//반복문으로 데이터 List를 추출해냄
+                    Data data = snapshot.getValue(Data.class);//만들어뒀던 User 객체에 데이터를 담는다
+                    //user클래스 안에다가 firebase db로부터 가져온 애를 userclass로 박아주고 걔를 arraylist에 담아다가 adapt로 쏘는
+                    arrayList.add(data);//실질적으로 넣고 arraylist에 추가해주면 완성. 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼준비
+                }
+                adapter.notifyDataSetChanged();//리스트 저장 및 새로고침
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
+                //디비를 가져오던중 에러 발생시
+                Log.e("MainActivity", String.valueOf(databaseError.toException()));//에러문 출력
+            }//입력을 안해도됨. 구현하는데 큰 문제 없음
         });
-
-
+        adapter = new DrugAdapter(arrayList, this);//adapter를 생성하고 그것을 arraylist에서 context를 넘겨줄게.
+        recylerView.setAdapter(adapter);//리사이클러뷰에 어뎁터연결
     }
 
-
-
-
-
 }
+
