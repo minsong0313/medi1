4ffc964efefe915d33e2258ceb365978512fe7db
Minsong Lee <minsong0313@naver.com>
2020-06-09 13:11:12 +0900

    realtime database.

--- app/src/main/java/com/example/medi1/DrugAdapter.java
+++ app/src/main/java/com/example/medi1/DrugAdapter.java
@@ -1,76 +1,60 @@
 package com.example.medi1;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
-
 import java.util.ArrayList;
+import java.util.Map;
 
-public class DrugAdapter extends RecyclerView.Adapter<DrugAdapter.DrugViewHolder> {
-    private ArrayList<Data> arrayList;
-    private Context context;
+public class DrugAdapter extends RecyclerView.Adapter<DrugAdapter.MyViewHolder> {
+    private ArrayList<Map<String, Object>> items= new ArrayList<Map<String, Object>>();
 
-    public DrugAdapter(ArrayList<Data> arrayList, Context context){
-        this.arrayList = arrayList;
-        this.context = context;
+    public DrugAdapter(ArrayList<Map<String, Object>> resultList){
+       this.items=resultList;
     }
 
     @NonNull
     @Override
-    public DrugViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        //실제 listview가 adapter에 연결된 다음에 이쪽에서 view holer를 최초로 만들어냄
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
-        DrugViewHolder holder = new DrugViewHolder(view);
-        return holder;
-        //최초 view에 대한 list item에 대한 view를 생성함.
-        //이 onBindViewHolder친구한테 실질적으로 매칭해주는 역할을 함.
+    public DrugAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item , parent, false);
+        return new MyViewHolder(itemView);
     }
 
+
     @Override
-    public void onBindViewHolder(@NonNull DrugViewHolder holder, int position) {
-        //각 item에 대한 매칭을함.
-        //이 세줄만 적어두면 이미지 view 안에 서버로부터 이미지를 받아와
-        //bind view holder될 때 넣어줄것임. 삽입될것임
-        Glide.with(holder.itemView)
-                .load(arrayList.get(position).getImage())
-                .into(holder.image);
-        //현재 position에 있는것을 가져와서 id를 그대로 입력해줘.
-        holder.name.setText(arrayList.get(position).getName());
-        holder.color.setText(String.valueOf(arrayList.get(position).getColor()));
-        holder.shape.setText(arrayList.get(position).getShape());
-
-        //arrayList가 User에 연결해놓았음. Mainactivity에서 firebase 데이터를 받아옴
-        //User객체가 있는 arrayList에다 담아서 adapter쪽으로 쏨
-        // 그러면 onBindViewHolder여기서 그 친구를 받아 glide로 load하는 그런 로직이됨
-
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Map<String, Object> item = (Map<String, Object>) items.get(position);
+        holder.setItem(item);
     }
 
     @Override
     public int getItemCount() {
-        return (arrayList != null ? arrayList.size() : 0);
+        return items.size();
     }
 
-    public class DrugViewHolder extends RecyclerView.ViewHolder{
-        ImageView image;
-        TextView name;
-        TextView color;
-        TextView shape;
-
-        public DrugViewHolder(@NonNull View itemView) {
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView name, image, shape, color;
+        public MyViewHolder(View itemView) {
             super(itemView);
-            this.image = itemView.findViewById(R.id.image);
-            this.name = itemView.findViewById(R.id.tv_name);
-            this.color = itemView.findViewById(R.id.tv_color);
-            this.shape = itemView.findViewById(R.id.tv_shape);
-        }
+            name=itemView.findViewById(R.id.name);
+            image=itemView.findViewById(R.id.image);
+            shape=itemView.findViewById(R.id.shape);
+            color=itemView.findViewById(R.id.color);
 
+        }
+        public void setItem(Map<String, Object> item){
+
+            //"rank", "movieNm", "openDt"은 Json파일에 저장되어 있던 key값
+            name.setText(item.get("name").toString());
+            image.setText(item.get("image").toString());
+            shape.setText(item.get("shape").toString());
+            color.setText(item.get("color").toString());
+
+        }
     }
 }
--- app/src/main/java/com/example/medi1/MainActivity.java
+++ app/src/main/java/com/example/medi1/MainActivity.java
@@ -1,66 +1,63 @@
 package com.example.medi1;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONObject;
+
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView recylerView;
-    private RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
-    private ArrayList<Data> arrayList;
-    private FirebaseDatabase database;
-    private DatabaseReference databaseReference;
+    private static final String TAG="Ma";
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+
+    Gson gson = new Gson();
+    ///////////////////////////색상 버튼////////////////////////////
+    public String color = null; //색상 저장
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recylerView = findViewById(R.id.recyclerView); //id연결
-        recylerView.setHasFixedSize(true);//리사이클러뷰 기존 성능 강화
-        layoutManager = new LinearLayoutManager(this);
-        recylerView.setLayoutManager(layoutManager);
-        arrayList = new ArrayList<>(); //user 객체 담을 array list(어뎁터 쪽으로 날리려고함)
+        recyclerView=findViewById(R.id.rv_recyclerview);
 
-        database = FirebaseDatabase.getInstance();
+        try{
+            InputStream is = getAssets().open("druglist3.json");
+            byte[] buffer = new byte[is.available()];
+            is.read(buffer);
+            is.close();
+            String json = new String(buffer, "UTF-8");
 
-        databaseReference = database.getReference("Druglist"); //DB테이블을 연결하는 작업
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                //파이어베이스 데이터베이스의 데이터를 받아오는 곳
-                arrayList.clear();//arraylist:유저 객체를 담을것. add한적도 없는데 clear로 초기화?
-                //기존 배열리스트가 존재하지 않게 초기화
-                for(DataSnapshot snapshot : dataSnapshot.getChildren()){//반복문으로 데이터 List를 추출해냄
-                    Data data = snapshot.getValue(Data.class);//만들어뒀던 User 객체에 데이터를 담는다
-                    //user클래스 안에다가 firebase db로부터 가져온 애를 userclass로 박아주고 걔를 arraylist에 담아다가 adapt로 쏘는
-                    arrayList.add(data);//실질적으로 넣고 arraylist에 추가해주면 완성. 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼준비
-                }
-                adapter.notifyDataSetChanged();//리스트 저장 및 새로고침
-            }
+            JSONObject jsonObject = new JSONObject(json);
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                //디비를 가져오던중 에러 발생시
-                Log.e("MainActivity", String.valueOf(databaseError.toException()));//에러문 출력
-            }//입력을 안해도됨. 구현하는데 큰 문제 없음
-        });
-        adapter = new DrugAdapter(arrayList, this);//adapter를 생성하고 그것을 arraylist에서 context를 넘겨줄게.
-        recylerView.setAdapter(adapter);//리사이클러뷰에 어뎁터연결
+            Map<String,Object> boxOfficeResult= gson.fromJson( jsonObject.get("boxOfficeResult").toString(),new TypeToken<Map<String, Object>>(){}.getType());
+
+            ArrayList<Map<String, Object>> jsonList = (ArrayList) boxOfficeResult.get("Drug");
+
+            mAdapter=new DrugAdapter(jsonList);
+
+        }catch (Exception e){e.printStackTrace();}
+
+    }//onCreate()..
+
+
+    public void click_btn(View view) {
+        recyclerView.setAdapter(mAdapter);
     }
 
+    //하양색 버튼을 눌러 색상이 하양인것을 출력하기
+    public void whiteclick(View view) {
+        color = "하양";
+        
+    }
 }
-
